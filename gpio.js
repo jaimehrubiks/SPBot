09557ef228e9fedd7298a42c3d21cdcefce4f7fe
jaimehrubiks <jaimehidalgo2@hotmail.com>
2017-12-19 17:03:29 +0000

    fixed servos offset

--- gpio.js
+++ gpio.js
@@ -6,9 +6,9 @@ const Gpio = require('pigpio').Gpio;
 
 // Servo_Configuration
 var servo = [{
-    io: new Gpio(2, {mode: Gpio.OUTPUT}), minPulse: 850, maxPulse: 2500}, {
-    io: new Gpio(3, {mode: Gpio.OUTPUT}), minPulse: 750, maxPulse: 2450}, {
-    io: new Gpio(4, {mode: Gpio.OUTPUT}), minPulse: 750, maxPulse: 2450}
+    io: new Gpio(2, {mode: Gpio.OUTPUT}), minPulse: 1200, maxPulse: 2000}, {							// 0: Cuerpo (850-2500)
+    io: new Gpio(3, {mode: Gpio.OUTPUT}), minPulse: 750,  maxPulse: 2450}, {							// 1: Brazo derecho (750-2450)
+    io: new Gpio(4, {mode: Gpio.OUTPUT}), minPulse: 750,  maxPulse: 2200 , invert: true} // 2: Brazo izquierdo (750-2450)
 ];
 
 // Leds_Configuracion
@@ -17,8 +17,8 @@ var servo = [{
 function move_servo(servo_n, range){
     if (range == -1) servo[servo_n].io.servoWrite(0);
     else if (range<0 || range>1) { console.log('Err. Servo range goes from 0 to 1'); return; }
+    if(servo[servo_n].invert) range = 1-range;
     range = Math.round( servo[servo_n].minPulse + range*(servo[servo_n].maxPulse-servo[servo_n].minPulse) );
-    // console.log('moving servo: ' + servo_n + ' to position: ' + range);
     servo[servo_n].io.servoWrite(range);
 }
 
